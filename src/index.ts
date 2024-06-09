0ecb49445bc2f86a18225b39a8137e1b0d81d978
lukegehron <luke@gehron.com>
2024-06-08 20:09:51 -0400

    final commit

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,36 @@ import {
   Viewer,
   DefaultViewerParams,
   SpeckleLoader,
-  MeasurementType,
+  ViewerEvent,
+  // SpeckleDepthMaterial
 } from "@speckle/viewer";
-import { CameraController, MeasurementsExtension } from "@speckle/viewer";
-import { makeMeasurementsUI } from "./MeasurementsUI";
+import { CameraController } from "@speckle/viewer";
+import { makeInputsUI } from "./InputsUI";
+import * as fal from "@fal-ai/serverless-client";
+
+
+
+async function runFAL() {
+
+  fal.config({
+    credentials: "YOUR_KEY_HERE",
+    proxyUrl: '/api/fal/proxy', // the built-int nextjs proxy
+    // proxyUrl: 'http://localhost:1234/api/fal/proxy', // or your own external proxy
+  });
+
+  const result = await fal.subscribe("comfy/lukegehron/speckle_ai", {
+  input: {
+    ksampler_seed: 123498235498246,
+    cliptextencode_text: "RAW Photo of a american house, high end, typical northeastern suburb, summer, exterior, early afternoon, 8k, film grain, high quality, fujifilm, dramatic sky, classic french countryside style, architecture rendering,",
+    image_load_image_path: "", 
+  },
+  logs: false,
+});
+  console.log(result);
+}
+
+console.log("Starting FAL");
+runFAL(); 
 
 async function main() {
   /** Get the HTML container */
@@ -13,29 +39,50 @@ async function main() {
 
   /** Configure the viewer params */
   const params = DefaultViewerParams;
+  console.log(params)
   params.verbose = true;
+
 
   /** Create Viewer instance */
   const viewer = new Viewer(container, params);
   /** Initialise the viewer */
   await viewer.init();
+  
 
   /** Add the stock camera controller extension */
   viewer.createExtension(CameraController);
-  /** Add mesurements extension */
-  const measurements = viewer.createExtension(MeasurementsExtension);
 
   /** Create a loader for the speckle stream */
   const loader = new SpeckleLoader(
     viewer.getWorldTree(),
     "https://latest.speckle.dev/streams/92b620fb17/objects/801360a35cd00c13ac81522851a13341",
+    // "https://app.speckle.systems/streams/eda93f4cb6/objects/8798429cec15e9bc5f6085f980ea272a",
     ""
   );
+  
   /** Load the speckle data */
   await viewer.loadObject(loader, true);
 
-  measurements.enabled = true;
-  makeMeasurementsUI(viewer);
+  async function displayImage() {
+    let result = await viewer.screenshot();
+    document.getElementById('image').src = result;
+    console.log(result)
+}
+  viewer.on(ViewerEvent.ObjectClicked, (event) => {
+    displayImage()
+  }
+  )
+
+  ViewerEvent.ObjectClicked
+  makeInputsUI(viewer);
+
+  setTimeout(() => {
+
+  viewer.resize()
+    let myScreenshot = viewer.screenshot();
+  console.log(myScreenshot)
+
+  }, 1000);
 }
 
 main();
